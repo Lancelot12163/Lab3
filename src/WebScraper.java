c55d2b131815287146954b3d4757137e1a7624e9
Yu Wang <yuw2@illinois.edu>
2018-02-06 18:30:41 -0600

    Started webscraper.

--- src/WebScraper.java
+++ src/WebScraper.java
@@ -1,5 +1,41 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
+
 public class WebScraper {
+    public static int wordCount(final String fromweb) {
+        int word = 0;
+        for (int i = 0; i < fromweb.length(); i++) {
+            if (fromweb.charAt(i) == ' ') {
+                word++;
+            }
+        }
+        return word;
+    }
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+    }
+
     public static void main(String[] unused) {
-        System.out.println("Here");
+        String web = urlToString("http://erdani.com/tdpl/hamlet.txt");
+        int wordintext = wordCount(web);
+        System.out.println(web);
+        System.out.println(wordintext);
+
     }
 }
